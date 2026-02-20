6b68845e07bb1114780157778ff056f59798d1b1
Grachev Yaroslav <grachev.yaroslav496@gmail.com>
2026-02-20 20:56:09 +0300

    add: service

--- internal/service/subscription.go
+++ internal/service/subscription.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"em-internship/internal/models"
 	"em-internship/internal/repository"
+	"fmt"
 
 	"github.com/go-playground/validator/v10"
 	"go.uber.org/zap"
@@ -11,7 +12,7 @@ import (
 
 type SubscriptionService struct {
 	repo      *repository.SubscriptionRepository
-	validator *validator.Validator
+	validator *validator.Validate
 	logger    *zap.Logger
 }
 
@@ -23,13 +24,37 @@ func NewSubscriptionService(repo *repository.SubscriptionRepository, logger *zap
 	}
 }
 
-func (s *SubscriptionService) Create(ctx context.Context, input models.CreateSubscriptionInput) (*models.Subscription, error)) {
+func (s *SubscriptionService) Create(ctx context.Context, input models.CreateSubscriptionInput) (*models.Subscription, error) {
 	if err := s.validator.StructCtx(ctx, input); err != nil {
 		s.logger.Warn("validation error", zap.Error(err))
-		return nil, fmt.Error("validation error", err)
+		return nil, fmt.Errorf("validation error: %w", err)
 	}
 
 	return s.repo.Create(ctx, input)
 }
 
-func
+func (s *SubscriptionService) GetByID(ctx context.Context, id string) (*models.Subscription, error) {
+	return s.repo.GetByID(ctx, id)
+}
+
+func (s *SubscriptionService) GetAll(ctx context.Context, limit, offset int) (*models.SubscriptionList, error) {
+	if limit <= 0 { // простая проверка на дурака
+		limit = 15
+	} else if limit > 99 {
+		limit = 99
+	}
+
+	return s.repo.GetAll(ctx, limit, offset)
+}
+
+func (s *SubscriptionService) Update(ctx context.Context, id string, input models.UpdateSubscriptionInput) (*models.Subscription, error) {
+	return s.repo.Update(ctx, id, input)
+}
+
+func (s *SubscriptionService) Delete(ctx context.Context, id string) error {
+	return s.repo.Delete(ctx, id)
+}
+
+func (s *SubscriptionService) GetTotalCostForPeriod(ctx context.Context, userID, serviceName, startDate, endDate string) (*models.TotalCostResponse, error) {
+	return s.repo.GetTotalCostForPeriod(ctx, userID, serviceName, startDate, endDate)
+}
